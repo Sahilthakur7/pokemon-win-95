e5a12bff56da6753d673816b35f6933048b6a16c
sahilthakur7 <sahilrd07@gmail.com>
2020-03-26 11:20:52 +0530

    Completed display of Appbar

--- pokemon-win-95/src/components/Menu.js
+++ pokemon-win-95/src/components/Menu.js
@@ -3,6 +3,7 @@ import { Button, List, ListItem, Divider } from "react95";
 
 const Menu = () => {
   const [showMenu, toggleShowMenu] = useState(false);
+  const sound = true;
 
   return (
     <div className="appbar-menu">
@@ -13,11 +14,32 @@ const Menu = () => {
           horizontalAlign="left"
           verticalAlign="bottom"
         >
-          <ListItem>Hello123</ListItem>
+          <ListItem className="start-list-item">
+            <img
+              src={require("../assets/cd.png")}
+              className="list-item-images"
+            />
+            <span className="list-item-text">Github Repo</span>
+          </ListItem>
+          <ListItem className="start-list-item">
+            <img
+              src={require("../assets/computer.png")}
+              className="list-item-images"
+            />
+            <span className="list-item-text">About</span>
+          </ListItem>
+          <Divider />
+          <ListItem className="start-list-item">
+            Startup Sound: {sound ? "On" : "Off"}
+          </ListItem>
         </List>
       )}
-      <Button onClick={e => toggleShowMenu(!showMenu)}>
-        <img src={require("../assets/windowslogo.png")} alt="winLogo" />
+      <Button onClick={e => toggleShowMenu(!showMenu)} active={showMenu}>
+        <img
+          src={require("../assets/windowslogo.png")}
+          alt="winLogo"
+          className="windows-logo"
+        />
         Pokemon 95
       </Button>
     </div>
