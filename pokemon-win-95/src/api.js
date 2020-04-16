ac0eff40fc86e86ea1de5ae64ae590c9decb20ba
sahilthakur7 <sahilrd07@gmail.com>
2020-04-16 16:18:16 +0530

    Started writing api to get pokemon

---
+++ pokemon-win-95/src/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const getAllPokemon = async () => {
+  try {
+    const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
+
+    const pokemonToReturn = res.data.results.map((pokemon, index) => {
+      return {
+        id: index + 1,
+        name: pokemon.name,
+        selected: false,
+      };
+    });
+
+    return pokemonToReturn;
+  } catch (err) {
+    console.log("some error occurred", err);
+  }
+};
+
+getAllPokemon();
+
+export { getAllPokemon };
